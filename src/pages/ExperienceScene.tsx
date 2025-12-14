64243e31ba0814e1c13f1ba7d64b5eed18e585a7
Jayp33333 <johnpauljamito5@gmail.com>
2025-12-14 12:12:46 +0800

    sample update

--- src/pages/ExperienceScene.tsx
+++ src/pages/ExperienceScene.tsx
@@ -1,5 +1,5 @@
 import { Canvas } from "@react-three/fiber";
-import { Viverse } from "@react-three/viverse";
+import { BvhPhysicsWorld, } from "@react-three/viverse";
 import Experience from "./Experience";
 import LoadingOverlay from "../components/ui/LoadingOverlay";
 import { UI } from "../components/ui/UI";
@@ -10,9 +10,9 @@ export default function ExperienceScene() {
       <LoadingOverlay />
       <UI />
       <Canvas style={{ position: "absolute", inset: 0, touchAction: "none" }}>
-        <Viverse clientId={import.meta.env.VITE_VIVERSE_APP_ID}>
+        <BvhPhysicsWorld>
           <Experience />
-        </Viverse>
+        </BvhPhysicsWorld>
       </Canvas>
     </>
   );
