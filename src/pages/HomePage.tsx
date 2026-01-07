0b15b1ce1fc547958860a562b69e6eeb5ccdd9a2
Jayp33333 <johnpauljamito5@gmail.com>
2026-01-07 21:16:16 +0800

    adding avatar and a little bit update

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,21 +4,10 @@ import { NavBar } from "../components/Home/NavBar";
 
 export default function HomePage() {
   return (
-    <div
-      style={{
-        background: "#0e0e0e",
-        color: "white",
-        position: "absolute",
-        inset: 0,
-      }}
-    >
+    <div className="bg-[#0e0e0e] text-white absolute inset-0">
       <NavBar />
-      {/* Hero Section */}
       <Hero />
-      {/* Feature Section */}
       <Features />
-
-      {/* About Section */}
     </div>
   );
 }
